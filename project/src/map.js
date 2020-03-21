6a2699e77b9b171955f27ed46dad2dbf99f91246
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-03-21 09:46:36 +0100

    correction of data points

--- project/src/map.js
+++ project/src/map.js
@@ -2,12 +2,16 @@
 const canvHeight = 800, canvWidth = 1200;
 const svg = d3.select("body").append("svg")
     .attr("width", canvWidth)
-    .attr("height", canvHeight)
+    .attr("height", canvHeight);
 
 const map = svg.append('image')
     .attr('xlink:href', './data/map_zh.svg')
     .attr("width", canvWidth)
     .attr("height", canvHeight)
+    .on("mousedown", function(d) {
+        console.log(event.clientX);
+        console.log(event.clientY);
+    });
 
 // calc the width and height depending on margins.
 const margin = {top: 50, right: 80, bottom: 50, left: 60};
@@ -29,6 +33,16 @@ const g = svg.append("g")
     .attr("id", "chart-area")
     .attr("transform", "translate(" +margin.left + "," + margin.top + ")");
 
+var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("padding", "6px")
+    .style("font-family", "sans-serif")
+    .style("color", "white")
+    .style("background-color", "#0066CC")
+
 d3.csv("./data/velo_fuss_zh_zaehler_pro_jahr.csv").then(function(data) {
     const xDomain = d3.extent(data, d => Number(d.x));
     const yDomain = d3.extent(data, d => Number(d.y));
@@ -41,15 +55,17 @@ d3.csv("./data/velo_fuss_zh_zaehler_pro_jahr.csv").then(function(data) {
         .data(data) // which is joined with the data
         .enter() // and a selection of new elements is created
         .append("circle")
-        .attr("cx", d => xScale((d.x)))
-        .attr("cy", d => yScale((d.y)))
+        .attr("cx", d => xScale(d.x)*0.33+295)
+        .attr("cy", d => yScale(d.y)*0.55+105)
         .attr("r", d => d.Mean_VELO_IN)
         .style("fill", "BLACK")
-        .on("mouseover", function(d) {
-            console.log('hovered');
-        })
-        .on("mouseout", function(d) {
-        });
-
-    ;
+        .on("mouseover", function(d){return tooltip.style("visibility", "visible").text(d.bezeichnung);})
+        .on("mousemove", function(d){
+            return tooltip
+                .style("top", (d3.event.pageY-10)+"px")
+                .style("left",(d3.event.pageX+10)+"px")
+                .text(d.bezeichnung + '\n(' + Math.round(d.Mean_VELO_IN*4) + ' Velos pro Stunde)');})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+        // .append("svg:title")
+        // .text(function(d) { return d.bezeichnung; });
 });
