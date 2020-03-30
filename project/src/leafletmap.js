d250ad1bc83e088b54bba33ff8c828bf5a21261b
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-03-30 10:15:49 +0200

    changed and added data, added leaflet map

---
+++ project/src/leafletmap.js
@@ -0,0 +1,42 @@
+var map = L.map('map').setView([47.3686498, 8.5391825], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    minZoom: 12,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZmFiaWFuYnVyIiwiYSI6ImNrOGQxcnVtdDBxdDEzcHFwN3BzZDh0d2sifQ.RZ1FydXU6FKtGo34haa5nQ'
+}).addTo(map);
+
+d3.csv("./data/velo_fuss_zh_zaehler_pro_jahr.csv").then(function(data) {
+    for (var i = 1; i < data.length; i++) {
+        var circle = L.circle([data[i].lat, data[i].lon], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: data[i].VeloInAvrg * 20
+        })
+            .bindPopup(data[i].bezeichnung + '<br>Velos/h: ' + Math.round(data[i].VeloInAvrg*4))
+            .addTo(map);
+    }
+});
+
+d3.csv("./data/zuerivelo_publibike.csv").then(function(data) {
+    for (var i = 1; i < data.length; i++) {
+        var circle = L.circle([data[i].lat, data[i].lon], {
+            color: 'blue',
+            fillColor: '#30f',
+            fillOpacity: 0.5,
+            radius: 50
+        })
+            .bindPopup(data[i].name)
+            .addTo(map);
+    }
+});
+
+map.setMaxBounds([
+    [47.33, 8.63],
+    [47.46, 8.47]
+]);
--- project/src/map.js
+++ project/src/map.js
@@ -86,16 +86,6 @@ const margin = {top: 50, right: 80, bottom: 50, left: 60};
 const width = canvWidth - margin.left - margin.right;
 const height = canvHeight - margin.top - margin.bottom;
 
-// // chart title
-// svg.append("text")
-//     .attr("y", 0)
-//     .attr("x", margin.left)
-//     .attr("dy", "1.5em")
-//     .attr("font-family", "sans-serif")
-//     .attr("font-size", "24px")
-//     .style("text-anchor", "left")
-//     .text("Zürich");
-
 // create parent group and add left and top margin
 const g = svg.append("g")
     .attr("id", "chart-area")
